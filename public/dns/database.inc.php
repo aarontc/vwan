496f152e53181bf8cb950411f0e2fc2c75fe00a4
aarontc@gmail.com <aarontc@gmail.com@bedd7fe4-1aae-11df-a76b-277a82a69c62>
2010-02-16 04:30:14 +0000

    oops

--- public/dns/database.inc.php
+++ public/dns/database.inc.php
@@ -1,5 +1,3 @@
 <?php
-	define ( '_SITE_PASSWORD_SALT_', "9uasdhf978h@#hewinfr3o87HNW(H@rsfda" );
-	$db = mysql_connect ( "localhost", "hyperbbq_webdns", "cb3taWNwTR,mdCZc" );
-	mysql_select_db ( "hyperbbq_com_dns" );
+	// replace with adodb :)
 ?>
