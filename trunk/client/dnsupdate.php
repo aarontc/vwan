7653a44c5427e038b7c32a6ef7eab233aa35d658
bjmn55@gmail.com <bjmn55@gmail.com@bedd7fe4-1aae-11df-a76b-277a82a69c62>
2010-03-30 19:28:55 +0000

    fixed an issue with dnsupdate.php and added some debug stuff

--- trunk/client/dnsupdate.php
+++ trunk/client/dnsupdate.php
@@ -1,5 +1,5 @@
 <?php
-// 	define('DEBUG', TRUE);
+	define('DEBUG', TRUE);
 	define('USERNAME', 'dummy');
 	define('PASSWORD', 'dummy');
 	$host = 'www.myvwan.com';
@@ -16,30 +16,34 @@
 	$f[] = explode("\n",file_get_contents("/etc/dhcp.leases"));
 	// collect statically mapped IP computers name and IP
 	$f[] = explode("\n",file_get_contents("/etc/hosts"));
-
+if(defined('DEBUG')) echo "Parsing dnsmasq.conf...";
 	foreach($f[0] as $fl) { // parse dnsmasq.conf
 		if(preg_match("/^domain=(.*)/", $fl, $domain)) {
 			$domain = $domain[1];
 			break;
 		}
 	}
-
-	$a = count($f[1]); $b = count($f[2]);
-	$l = $a > $b ? $a : $b;
+if(defined('DEBUG')) echo "done.\n";
+	$a = count($f[1]);
+	$b = count($f[2]);
 
 	$doc = new DOMDocument();
 	$doc->formatOutput = true;
 	$r = $doc->createElement( "zone" );
 	$r->setAttribute("name", "$domain");
 	$doc->appendChild( $r );
-
-	for($i = 0; $i < $l; $i++) {
-		if($i < $a && strlen($f[1][$i]) > 0) { // parse dhcp.leases
+if(defined('DEBUG')) echo "Parsing dhcp.leases...";
+	for($i = 0; $i < $a; $i++) {
+		if(strlen($f[1][$i]) > 0) { // parse dhcp.leases
 			$p = preg_split("/[\s]+/", $f[1][$i], -1, PREG_SPLIT_NO_EMPTY);
 			// build XML here
 			if($p[3] != "*") appendToXmlDoc($doc, $r, "a", $p[3], $p[2]);
 		}
-		if($i < $b && strlen($f[2][$i]) > 0 && !preg_match("/^[\s]*#/", $f[2][$i])) { // parse hosts
+	}
+if(defined('DEBUG')) echo "done.\n";
+if(defined('DEBUG')) echo "Parsing hosts...";
+	for($i = 0; $i < $b; $i++) {
+		if(strlen($f[2][$i]) > 0 && !preg_match("/^[\s]*#/", $f[2][$i])) { // parse hosts
 			$p = preg_split("/[\s]+/", $f[2][$i], -1, PREG_SPLIT_NO_EMPTY);
 			if(!preg_match("/^127.*|^\:\:1$/", $p[0])) { // make sure we haven't just found the loopback entry
 				$found_host_name = false;
@@ -58,11 +62,11 @@
 			}
 		}
 	}
-
+if(defined('DEBUG')) echo "done.\n";
 	$data = "login=".urlencode(USERNAME);
 	$data .= "&password=".urlencode(PASSWORD);
 	$data .= "&dnsxml=".urlencode($doc->saveXML());
-	
+if(defined('DEBUG')) echo "Sending info...";
 	$sslhost = $use_ssl ? "ssl://$host" : $host;
 	$fp = fsockopen($sslhost,$port,$errno,$errstr,$timeout) or die("Error: ".$errstr.$errno);
 	fputs($fp, "POST $path HTTP/1.1\r\n");
@@ -72,7 +76,7 @@
 	fputs($fp, "Content-length: ".strlen($data)."\r\n");
 	fputs($fp, "Connection: close\r\n\r\n");
 	fputs($fp, $data);
-
+if(defined('DEBUG')) echo "done.\n";
 	while(!feof($fp)) $d .= fgets($fp,4096);
 // 	while(!feof($fp)) {
 // 		$d = fgets($fp,4096);
@@ -81,7 +85,11 @@
 // 		}
 // 	}
 	fclose($fp);
-	echo $d;
+if(!defined('DEBUG') || strlen($d) > 0) {
+	echo "got it.\n", $d;
+} else {
+	echo "no response from server!\n";
+}
 	
 	function appendToXmlDoc($doc, $r, $type, $name, $data) {
 		$b = $doc->createElement( "rr" );
