8a86266a650cecf2d95e2c6c32f87aa906c6fc7e
bjmn55@gmail.com <bjmn55@gmail.com@bedd7fe4-1aae-11df-a76b-277a82a69c62>
2010-03-30 19:46:36 +0000

    changed the define('DEBUG') to the more applicable $be_verbose and added the ability to print the XML that's about to be sent

--- client/dnsupdate.php
+++ client/dnsupdate.php
@@ -1,5 +1,5 @@
 <?php
-	define('DEBUG', TRUE);
+// 	define('DEBUG', TRUE);
 	define('USERNAME', 'dummy');
 	define('PASSWORD', 'dummy');
 	$host = 'www.myvwan.com';
@@ -8,6 +8,9 @@
 	$use_ssl = true;
 	$timeout = 30; // connection timeout
 	$useragent = 'MyVWAN DNS Updater/0.1';
+	
+	$be_verbose = true;
+	$print_xml = true;
 
 	$boxes = array();
 	// get the domain name
@@ -16,14 +19,16 @@
 	$f[] = explode("\n",file_get_contents("/etc/dhcp.leases"));
 	// collect statically mapped IP computers name and IP
 	$f[] = explode("\n",file_get_contents("/etc/hosts"));
-if(defined('DEBUG')) echo "Parsing dnsmasq.conf...";
+	
+	if($be_verbose) echo "Parsing dnsmasq.conf...";
 	foreach($f[0] as $fl) { // parse dnsmasq.conf
 		if(preg_match("/^domain=(.*)/", $fl, $domain)) {
 			$domain = $domain[1];
 			break;
 		}
 	}
-if(defined('DEBUG')) echo "done.\n";
+	if($be_verbose) echo "done.\n";
+	
 	$a = count($f[1]);
 	$b = count($f[2]);
 
@@ -32,7 +37,8 @@ if(defined('DEBUG')) echo "done.\n";
 	$r = $doc->createElement( "zone" );
 	$r->setAttribute("name", "$domain");
 	$doc->appendChild( $r );
-if(defined('DEBUG')) echo "Parsing dhcp.leases...";
+	
+	if($be_verbose) echo "Parsing dhcp.leases...";
 	for($i = 0; $i < $a; $i++) {
 		if(strlen($f[1][$i]) > 0) { // parse dhcp.leases
 			$p = preg_split("/[\s]+/", $f[1][$i], -1, PREG_SPLIT_NO_EMPTY);
@@ -40,8 +46,9 @@ if(defined('DEBUG')) echo "Parsing dhcp.leases...";
 			if($p[3] != "*") appendToXmlDoc($doc, $r, "a", $p[3], $p[2]);
 		}
 	}
-if(defined('DEBUG')) echo "done.\n";
-if(defined('DEBUG')) echo "Parsing hosts...";
+	if($be_verbose) echo "done.\n";
+	
+	if($be_verbose) echo "Parsing hosts...";
 	for($i = 0; $i < $b; $i++) {
 		if(strlen($f[2][$i]) > 0 && !preg_match("/^[\s]*#/", $f[2][$i])) { // parse hosts
 			$p = preg_split("/[\s]+/", $f[2][$i], -1, PREG_SPLIT_NO_EMPTY);
@@ -62,11 +69,15 @@ if(defined('DEBUG')) echo "Parsing hosts...";
 			}
 		}
 	}
-if(defined('DEBUG')) echo "done.\n";
+	if($be_verbose) echo "done.\n";
+	if($print_xml)
+		echo "#### XML to be sent ####\n", $doc->saveXML(), "########################\n";
+	
 	$data = "login=".urlencode(USERNAME);
 	$data .= "&password=".urlencode(PASSWORD);
 	$data .= "&dnsxml=".urlencode($doc->saveXML());
-if(defined('DEBUG')) echo "Sending info...";
+	
+	if($be_verbose) echo "Sending info...";
 	$sslhost = $use_ssl ? "ssl://$host" : $host;
 	$fp = fsockopen($sslhost,$port,$errno,$errstr,$timeout) or die("Error: ".$errstr.$errno);
 	fputs($fp, "POST $path HTTP/1.1\r\n");
@@ -76,7 +87,9 @@ if(defined('DEBUG')) echo "Sending info...";
 	fputs($fp, "Content-length: ".strlen($data)."\r\n");
 	fputs($fp, "Connection: close\r\n\r\n");
 	fputs($fp, $data);
-if(defined('DEBUG')) echo "done.\n";
+	if($be_verbose) echo "done.\n";
+	
+	if($be_verbose) echo "Waiting for server response...";
 	while(!feof($fp)) $d .= fgets($fp,4096);
 // 	while(!feof($fp)) {
 // 		$d = fgets($fp,4096);
@@ -85,11 +98,13 @@ if(defined('DEBUG')) echo "done.\n";
 // 		}
 // 	}
 	fclose($fp);
-if(!defined('DEBUG') || strlen($d) > 0) {
-	echo "got it.\n", $d;
-} else {
-	echo "no response from server!\n";
-}
+	if($be_verbose) {
+		if(strlen($d) > 0)
+			echo "got it.\n";
+		else
+			echo "no response from server!\n";
+	}
+	echo $d;
 	
 	function appendToXmlDoc($doc, $r, $type, $name, $data) {
 		$b = $doc->createElement( "rr" );
